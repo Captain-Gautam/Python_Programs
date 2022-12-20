1741160fb9b664f7ae631617e5d086e6f30ae257
Captain-Gautam <gautamprajapati2252@gmail.com>
2022-12-20 16:53:58 +0530

    It combines two string together.

---
+++ Band Generator Name.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#"""
+#Created on Tue Apr  5 18:49:23 2022
+
+#@author: gautam
+#"""
+
+# BAnd Generator Name
+
+print("Welcome to Band Generator Name:")
+
+n1 = input("What's your city name you grow up in?\n")
+
+n2 = input("What's your pet's name?\n")
+
+print("Your Band Name could be", n1,n2) 
+
