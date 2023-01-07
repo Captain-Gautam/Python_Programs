4ad785a9514cd482e7eb3fc5397dfa7d5d025ecc
Captain-Gautam <gautamprajapati2252@gmail.com>
2023-01-07 16:19:02 +0530

    It checks the year enter by user is leap or not

---
+++ Check_leapyear.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 16 08:35:45 2022
+
+@author: gautam
+"""
+input_year = int(input("Enter the Year to be checked: "))
+if (input_year%400 == 0):
+          print("Given year is a Leap Year")
+elif (input_year%100 == 0):
+          print("Given year is a not the Leap Year")
+elif (input_year%4 == 0):
+          print("Given year is a Leap Year")
+else:
+          print("%d is Not the Leap Year")
